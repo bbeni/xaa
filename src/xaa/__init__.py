8cc7ad3fd477df09f973193b2c8915c752468d2f
Benjamin Froelich <bbenif@gmail.com>
2024-02-16 23:26:43 +0100

    added some default imports

---
+++ src/xaa/__init__.py
@@ -0,0 +1,6 @@
+from . import plotting
+from .core import SingleMeasurementProcessor as Processor
+from . import helpers
+from . import operations
+from . import xmcd_sumrules
+from .loaders.util import boreas_file_to_dataframes as read_dat_file
